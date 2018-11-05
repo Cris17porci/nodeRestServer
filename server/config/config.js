2da2bf39cefccee2b0f42059febcb5227da2383c
Cristobal <Cristobal.perez@ceinf.cl>
2018-11-04 21:59:34 -0300

    Rest server Usuario new

--- server/config/config.js
+++ server/config/config.js
@@ -9,11 +9,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
     // ==============
     //  Entorno
     // ==============
-    /* let urlDB;
-    if (process.env.NODE_ENV === 'dev') {
-        urlDB = 'mongodb://localhost:27017/botilleria'
-    } else {
-        urlDB = 'mongodb://cperezgo:depto1206!@cluster0-shard-00-00-kupno.mongodb.net:27017,cluster0-shard-00-01-kupno.mongodb.net:27017,cluster0-shard-00-02-kupno.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
-    }
-    process.env.URLDB = urlDB */
-process.env.URLDB = 'mongodb://cperezgo:depto1206!@cluster0-shard-00-00-kupno.mongodb.net:27017,cluster0-shard-00-01-kupno.mongodb.net:27017,cluster0-shard-00-02-kupno.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/botilleria'
+} else {
+    urlDB = 'mongodb://cperezgo:depto1206!@cluster0-shard-00-00-kupno.mongodb.net:27017,cluster0-shard-00-01-kupno.mongodb.net:27017,cluster0-shard-00-02-kupno.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+}
+process.env.URLDB = urlDB
